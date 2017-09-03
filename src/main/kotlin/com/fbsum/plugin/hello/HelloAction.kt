c2f1b1e29305ee021df5e632708167b305455ed5
sum <scausum@163.com>
2017-09-03 22:49:48 +0800

    Add log test

--- src/main/kotlin/com/fbsum/plugin/hello/HelloAction.kt
+++ src/main/kotlin/com/fbsum/plugin/hello/HelloAction.kt
@@ -2,11 +2,20 @@ package com.fbsum.plugin.hello
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.ui.Messages
 
 class HelloAction : AnAction() {
 
+    private val log = Logger.getInstance(HelloAction::class.java)
+
     override fun actionPerformed(e: AnActionEvent) {
+
+        log.info("actionPerformed()")
+        log.warn("actionPerformed()")
+        log.debug("actionPerformed()")
+        log.error("actionPerformed()")
+
         val project = e.project
         val s = Messages.showInputDialog(project, "What's your name?", "Hello", Messages.getQuestionIcon())
         Messages.showMessageDialog(project, "Hello $s!", "Welcome", Messages.getInformationIcon())
